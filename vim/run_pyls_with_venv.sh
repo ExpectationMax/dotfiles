433e824eba5b83dc01cc2f538af0406d543596b7
Max Horn <max.horn@bsse.ethz.ch>
2019-07-16 14:04:57 +0200

    Fixes with pylint

--- vim/run_pyls_with_venv.sh
+++ vim/run_pyls_with_venv.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PIPENV_VIRTUAL_ENV=$(pipenv --venv 2>/dev/null)
+PIPENV_VIRTUAL_ENV=$(PIPENV_IGNORE_VIRTUALENVS=1 pipenv --venv 2>/dev/null)
 if [ -z "${PIPENV_VIRTUAL_ENV}" ]; then
    ~/.neovim_venv/bin/python3 -m pyls "$@" 2> ~/.vim/pyls_errors.log
 else
