da413bd0768b0984becc316da3c3a9179dc42a26
Max Horn <max.horn@bsse.ethz.ch>
2019-07-16 13:48:48 +0200

    Fixed path to pyls in run_pyls_in_venv.sh

--- vim/run_pyls_with_venv.sh
+++ vim/run_pyls_with_venv.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 PIPENV_VIRTUAL_ENV=$(pipenv --venv 2>/dev/null)
 if [ -z "${PIPENV_VIRTUAL_ENV}" ]; then
-   /usr/local/bin/python3 -m pyls "$@" 2> ~/.vim/pyls_errors.log
+   ~/.neovim_venv/bin/python3 -m pyls "$@" 2> ~/.vim/pyls_errors.log
 else
    export VIRTUAL_ENV=$PIPENV_VIRTUAL_ENV
-   /usr/local/bin/python3 -m pyls "$@" 2> ~/.vim/pyls_errors.log
+   ~/.neovim_venv/bin/python3 -m pyls "$@" 2> ~/.vim/pyls_errors.log
 fi
 
