3980125ab400d816401cd49b60e98294cb52c648
Max Horn <max.horn@bsse.ethz.ch>
2019-10-23 14:11:36 +0200

    Add support for virtualenvs created with poetry

--- vim/run_pyls_with_venv.sh
+++ vim/run_pyls_with_venv.sh
@@ -1,9 +1,14 @@
-#!/bin/sh
-PIPENV_VIRTUAL_ENV=$(PIPENV_IGNORE_VIRTUALENVS=1 pipenv --venv 2>/dev/null)
-if [ -z "${PIPENV_VIRTUAL_ENV}" ]; then
+#!/bin/bash
+DETECTED_VIRTUAL_ENV=$(PIPENV_IGNORE_VIRTUALENVS=1 pipenv --venv 2>/dev/null)
+# If we dont find a pipenv environemnt try poetry
+if [ -z "${DETECTED_VIRTUAL_ENV}" ]; then
+    DETECTED_VIRTUAL_ENV=$(poetry show -v 2>/dev/null| head -n 1 | sed 's/Using virtualenv: //')
+fi
+
+if [ -z "${DETECTED_VIRTUAL_ENV}" ]; then
    ~/.neovim_venv/bin/python3 -m pyls "$@" 2> ~/.vim/pyls_errors.log
 else
-   export VIRTUAL_ENV=$PIPENV_VIRTUAL_ENV
+   export VIRTUAL_ENV=$DETECTED_VIRTUAL_ENV
    ~/.neovim_venv/bin/python3 -m pyls "$@" 2> ~/.vim/pyls_errors.log
 fi
 
