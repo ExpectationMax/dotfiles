403ba30dc95a48d31e5214b7b40f60e0f756de75
Max Horn <maexlich@gmail.com>
2023-11-13 23:41:05 +0100

    Move on_attach to utils so it can be defined when configuring zk-nvim

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,5 +1,6 @@
 local os = require("os")
 local utils = require("expectationmax.utils")
+local on_attach = utils.on_attach
 local cmp = require("cmp")
 local luasnip = require("luasnip")
 local cmp_select_opts = {behavior = cmp.SelectBehavior.Insert}
@@ -84,76 +85,11 @@ cmp.setup({
 })
 
 local lspconfig = require("lspconfig")
-local configs = require("lspconfig/configs")
-
-local function on_attach(client, bufnr)
-    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-    local opts = { noremap=true, silent=true }
-
-    buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    -- buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    buf_set_keymap("i", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    buf_set_keymap("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-    buf_set_keymap("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-    buf_set_keymap("n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-    buf_set_keymap("n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-    buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-    buf_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-    buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    buf_set_keymap("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-    buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-    buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-    buf_set_keymap("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-    buf_set_keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
-    vim.opt_local.tagfunc = "v:lua.vim.lsp.tagfunc"
-
-    if client.server_capabilities.documentHighlight ~= nil then
-        vim.api.nvim_create_autocmd("CursorHold", {
-            buffer = bufnr,
-            callback = vim.lsp.buf.document_highlight
-        })
-        vim.api.nvim_create_autocmd("CursorHoldI", {
-            buffer = bufnr,
-            callback = vim.lsp.buf.document_highlight
-        })
-        vim.api.nvim_create_autocmd("CursorMoved", {
-            buffer = bufnr,
-            callback = vim.lsp.buf.clear_references
-        })
-    end
-
-    
-    vim.api.nvim_set_hl(0, "LspReference", {
-        bg = "#665c54",
-        ctermbg = 59,
-    })
-    vim.api.nvim_set_hl(0, "LspReferenceText", {
-        link = "LspReference",
-    })
-    vim.api.nvim_set_hl(0, "LspReferenceRead", {
-        link = "LspReference",
-    })
-    vim.api.nvim_set_hl(0, "LspReferenceWrite", {
-        link = "LspReference",
-    })
-    --[[
-    vim.api.nvim_create_autocmd("CursorHold", {
-        buffer = bufnr,
-        callback = function ()
-            vim.diagnostic.open_float({ focusable = false })
-        end
-    })
-    --]]
-end
 
 local function project_root_or_cur_dir(path)
     return lspconfig.util.root_pattern("pyproject.toml", "Pipfile", ".git")(path) or vim.fn.getcwd()
 end
+
 
 local lsp_defaults = lspconfig.util.default_config
 
--- nvim/lua/expectationmax/plugins/zk.lua
+++ nvim/lua/expectationmax/plugins/zk.lua
@@ -1,11 +1,14 @@
 return {
     "mickael-menu/zk-nvim",
     config = function()
+        local on_attach = require("expectationmax.utils").on_attach
         require("zk").setup({
             picker = "telescope",
+            lsp = { config = { on_attach = on_attach } }
         })
         require("telescope").load_extension("zk")  -- Activate zk extension
     end,
+    ft = { "markdown" },
     keys = {
         {"<leader>zn", function() require("zk").new({ title = vim.fn.input("Title: "), dir = "zettel" }) end, desc = "Create a new zettel note"},
         {"<leader>zm", function() require("zk").new({ title = vim.fn.input("Title: "), dir = "MOC" }) end, desc = "Create a new MOC note"},
