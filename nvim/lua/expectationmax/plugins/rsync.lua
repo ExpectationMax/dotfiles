7073fd8b4542bedd49425f9992c7f1d3242d291b
Max Horn <max.x.horn@gsk.com>
2023-11-13 12:06:54 +0100

    Add rsync package

---
+++ nvim/lua/expectationmax/plugins/rsync.lua
@@ -0,0 +1,13 @@
+return {
+    "OscarCreator/rsync.nvim",
+    build = "make",
+    dependencies = {"nvim-lua/plenary.nvim"},
+    opts = {
+        -- triggers sync when git repo was changed
+        fugitive_sync = true,
+        -- triggers `RsyncUp` when you save a file.
+        sync_on_save = true
+    },
+    keys = { {"<leader>pu", vim.cmd.RsyncUp, desc="Project upload via rsync"} },
+    cmd = {"RsyncUp", "RsyncUpFile"}
+}
