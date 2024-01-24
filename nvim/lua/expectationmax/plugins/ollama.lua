555a6e83431f602311273faad73ba5b108afc91b
Max Horn <maexlich@gmail.com>
2024-01-24 16:42:46 +0100

    Add ollama plugin

---
+++ nvim/lua/expectationmax/plugins/ollama.lua
@@ -0,0 +1,32 @@
+return {
+  "nomnivore/ollama.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+
+  -- All the user commands added by the plugin
+  cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
+
+  keys = {
+    -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
+    {
+      "<leader>op",
+      ":<c-u>lua require('ollama').prompt()<cr>",  
+      desc = "Prompt",
+      mode = { "n", "v" },
+    },
+
+    -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
+    {
+      "<leader>oG",
+      ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
+      desc = "Generate Code",
+      mode = { "n", "v" },
+    },
+  },
+
+  opts = {
+    model = "codellama"
+    -- your configuration overrides
+  }
+}
