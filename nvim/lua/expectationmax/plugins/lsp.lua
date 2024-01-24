ae0cbf89970d6512f7d2e081cd4164c3261be17c
Max Horn <maexlich@gmail.com>
2024-01-24 16:41:26 +0100

    Add mason

--- nvim/lua/expectationmax/plugins/lsp.lua
+++ nvim/lua/expectationmax/plugins/lsp.lua
@@ -1,5 +1,7 @@
 return {
-    "neovim/nvim-lspconfig",
+    {"williamboman/mason.nvim", config=true},
+    {"williamboman/mason-lspconfig.nvim", config=true},
+    {"neovim/nvim-lspconfig"},
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-nvim-lua",
     "L3MON4D3/LuaSnip",
