1795cc4b2cc1fcad17bb351d6d725b2e9215d800
ExpectationMax <1303655+ExpectationMax@users.noreply.github.com>
2025-03-20 15:48:23 +0100

    Remove lsp status from lualine

--- nvim/lua/expectationmax/plugins/lualine.lua
+++ nvim/lua/expectationmax/plugins/lualine.lua
@@ -2,15 +2,15 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = {
         {'nvim-tree/nvim-web-devicons', lazy = true },
-        {'arkav/lualine-lsp-progress', lazy = true },
+        -- {'arkav/lualine-lsp-progress', lazy = true },
     },
     opts = {
         options = {
             theme = "tokyonight",
             globalstatus = true
         },
-        sections = {
-            lualine_c = {"lsp_progress"}
-        }
+        -- sections = {
+        --     lualine_c = {"lsp_progress"}
+        -- }
     }
 }
